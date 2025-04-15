3dddbe6d6e73066b955b5a1614ac7fca2abd5cf8
Lasse Vågsæther Karlsen <lasse@vkarlsen.no>
2025-04-15 13:42:22 +0200

    Scripts for deployment

---
+++ start.ps1
@@ -0,0 +1,3 @@
+$Env:ASPNETCORE_URLS="http://0.0.0.0:7208/"
+$Env:ASPNETCORE_ENVIRONMENT="Production"
+dotnet /home/lassevk/Apps/EsoWatch/run/EsoWatch.dll
---
+++ update.ps1
@@ -0,0 +1,5 @@
+# stop launchctl
+
+dotnet publish EsoWatch/EsoWatch.csproj -c Release ./run
+
+# start launchctl
